59f5758b3f3985cf262fd4753c66371898897172
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-27 22:34:02 +0200

    path module to load html file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello world!!!</h1>`)
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
