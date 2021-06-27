87d9e9082992b1c1705b54951647c2a02b872f7b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-27 22:24:53 +0200

    response at localhost:PORT

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello world!!!</h1>`)
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
