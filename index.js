fcf3119543e7f80b5b6ca6009f65cc476f0c7cf6
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-27 23:40:55 +0200

    install prettier

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { log } = require('console');
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
 const members = require('./Members');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
 const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  console.log(
+    `${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`
+  );
   next();
-}
+};
 
 // initialise middleware
 app.use(logger);
