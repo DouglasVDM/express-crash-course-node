1ee6addd7fc981c4440b2cc77c4f0b6456b2548e
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-28 00:52:23 +0200

    members api routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { log } = require('console');
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -11,24 +10,10 @@ const PORT = process.env.PORT || 5000;
 // initialise middleware
 // app.use(logger);
 
-
-// get all members as json
-app.get('/api/members', (req, res) => {
-  res.json(members)
-});
-
-// get single member
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({msg: `No member with the id of ${req.params.id}`})
-  }
-});
-
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api routes
+app.use('/api/members', require('./routes/api/members'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
