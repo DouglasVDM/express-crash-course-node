88e29503313cca8de18756170067c94d09874c92
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-27 22:47:28 +0200

    route to array return json

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+
+const members = [
+  {
+    id: 1,
+    name: 'Morem Hpsum',
+    email: 'moremhpsum2gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Lorem Ipsum',
+    email: 'loremipsum2gmail.com',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'Borem Jpsum',
+    email: 'boremjpsum2gmail.com',
+    status: 'active'
+  }
+];
+
+app.get('/api/members', (req, res) => {
+  res.json(members)
+});
+
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
